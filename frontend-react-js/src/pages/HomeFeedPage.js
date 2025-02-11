a3ffd596f9b35b28205e3b722bc91a1c044c9fc9
Séun Olarinre <seun747@gmail.com>
2025-02-11 23:49:17 +0000

    Modified backend and frontend url to get it working on local host

--- frontend-react-js/src/pages/HomeFeedPage.js
+++ frontend-react-js/src/pages/HomeFeedPage.js
@@ -21,6 +21,7 @@ export default function HomeFeedPage() {
   const loadData = async () => {
     try {
       const backend_url = `${process.env.REACT_APP_BACKEND_URL}/api/activities/home`
+      console.log(`backend_url: ${backend_url}`)
       const res = await fetch(backend_url, {
         method: "GET"
       });
